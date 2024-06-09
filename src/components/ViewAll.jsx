b85afe9389d29e7b5a9a4161a1399611d84ab780
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-09 23:55:47 +0530

    changes

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,41 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewAll = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("https://anishpdm.github.io/dummy-api-new/student.json");
+            setData(response.data);
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+        }
+    };
     return (
         <div>
-            <NavBar />
             <div className="container">
                 <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <table class="table">
+                    <div className="col-12">
+                        <table className="table table-striped">
                             <thead>
                                 <tr>
-
-                                    <th scope="col">id</th>
-                                    <th scope="col">firstName</th>
-                                    <th scope="col">lastname</th>
-                                    <th scope="col">college</th>
-                                    <th scope="col">dob</th>
-                                    <th scope="col">course</th>
-                                    <th scope="col">mobile</th>
-                                    <th scope="col">email</th>
-                                    <th scope="col">address</th>
-                                    <th scope="col">V</th>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">First Name</th>
+                                    <th scope="col">Last Name</th>
+                                    <th scope="col">College</th>
+                                    <th scope="col">Dob</th>
+                                    <th scope="col">Course</th>
+                                    <th scope="col">Mobile</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Address</th>
+                                    <th scope="col">v</th>
                                 </tr>
                             </thead>
                             <tbody>
-
+                                {data.map((value, index) => (
+                                    <tr key={index}>
+                                        <th scope="row">{value._id}</th>
+                                        <td>{value.firstname}</td>
+                                        <td>{value.lastname}</td>
+                                        <td>{value.college}</td>
+                                        <td>{value.dob}</td>
+                                        <td>{value.course}</td>
+                                        <td>{value.mobile}</td>
+                                        <td>{value.email}</td>
+                                        <td>{value.address}</td>
+                                        <td>{value.__v}</td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
-
-
-
-        </div>
-    )
-}
+        </div >
+    );
+};
 
 export default ViewAll
