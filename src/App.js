1951eeee26394bf370c046d9aee2247ad93a47d0
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-09 20:20:54 +0530

    changes in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddStudent from './components/AddStudent';
+import SearchStudent from './components/SearchStudent';
 
 function App() {
   return (
     <div>
       <AddStudent />
+      <SearchStudent />
     </div>
   );
 }
