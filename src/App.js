d15fd757c2ded4517ed07511cf28c61cef37b6bd
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-09 21:12:08 +0530

    changes in project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddStudent from './components/AddStudent';
 import SearchStudent from './components/SearchStudent';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
       <AddStudent />
       <SearchStudent />
+      <ViewAll />
     </div>
   );
 }
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+
+                                    <th scope="col">id</th>
+                                    <th scope="col">firstName</th>
+                                    <th scope="col">lastname</th>
+                                    <th scope="col">college</th>
+                                    <th scope="col">dob</th>
+                                    <th scope="col">course</th>
+                                    <th scope="col">mobile</th>
+                                    <th scope="col">email</th>
+                                    <th scope="col">address</th>
+                                    <th scope="col">V</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default ViewAll
