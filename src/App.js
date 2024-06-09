5d3e4c8afd655299a3a8df7fbf19bcbf338bf620
Anjaleekrishna135 <anjaleekrishna135@gmail.com>
2024-06-09 23:03:43 +0530

    changes occured

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import AddStudent from './components/AddStudent';
 import SearchStudent from './components/SearchStudent';
 import ViewAll from './components/ViewAll';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddStudent />
-      <SearchStudent />
-      <ViewAll />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AddStudent />} />
+        <Route path='/search' element={<SearchStudent />} />
+        <Route path='/viewall' element={<ViewAll />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div>
         <nav className="navbar navbar-expand-lg bg-warning">
     <div className="container-fluid">
-        <a className="navbar-brand" href="#">FISAT</a>
+        <Link className="navbar-brand" to="#">FISAT</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-                <a className="nav-link active" aria-current="page" to="/">Home</a>
-                <a className="nav-link" href="/search">Search</a>
-                <a className="nav-link" href="/view">View All</a>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link" to="/search">Search</Link>
+                <Link className="nav-link" to="/viewall">View All</Link>
 
             </div>
         </div>
